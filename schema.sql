2cb1856bacf1338ce64775f6282a3df85fc660f9
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2023-02-05 22:32:49 +0200

    New table to schema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,11 @@ CREATE TABLE users (
     password TEXT,
     is_admin BOOLEAN
 );
+
+CREATE TABLE restaurants (
+    id SERIAL PRIMARY KEY,
+    name TEXT, 
+    address TEXT UNIQUE,
+    price_range INT,
+    category TEXT
+);
