1891b5be9b9b6fd1acb80754fe52d99736909490
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2023-02-05 22:32:14 +0200

    Created add new restaurant feature

--- restaurants.py
+++ restaurants.py
@@ -1,1 +1,13 @@
 from database import db
+from sqlalchemy.sql import text
+
+def new_restaurant(name, address, price_range, category):
+    try:
+        sql = text("""INSERT INTO restaurants (name, address, price_range, category) VALUES (:name, :address, :price_range, :category)""")
+        db.session.execute(sql, {"name":name, "address":address, "price_range":price_range, "category":category})
+        db.session.commit()
+        return True
+    except:
+        return False
+
+    
