3fa483753adc68a0df79a54facc8cbf8155cab83
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2023-03-05 16:21:28 +0200

    Added functionality

--- restaurants.py
+++ restaurants.py
@@ -10,20 +10,26 @@ def new_restaurant(name, address, price_range, category, description):
     except:
         return False
 
-def add_opening_hours(restaurant_id, day, open, close):
+def add_opening_hours(restaurant_id, day, open, close, status):
     try:
-        sql = text("""INSERT INTO opening_hours (restaurant_id, day, open, close) VALUES (:restaurant_id, :day, :open, :close)""")
-        db.session.execute(sql, {"restaurant_id":restaurant_id, "day":day, "open":open, "close":close})
+        sql = text("""INSERT INTO opening_hours (restaurant_id, day, open, close, status) VALUES (:restaurant_id, :day, :open, :close, :status)""")
+        db.session.execute(sql, {"restaurant_id":restaurant_id, "day":day, "open":open, "close":close, "status":status})
         db.session.commit()
         return True
     except:
         return False
 
-def get_short_info():
-    sql = text("SELECT id, name, address FROM restaurants")
+def get_short_info(sort="name"):
+    if sort == "name":
+        sql = text("SELECT id, name, address FROM restaurants ORDER BY name")
+    if sort == "lowest_price":
+        sql = text("SELECT id, name, address FROM restaurants ORDER BY price_range")
+    if sort == "highest_price":
+        sql = text("SELECT id, name, address FROM restaurants ORDER BY price_range DESC")
+    if sort == "rating":
+        sql = text("SELECT res.id, res.name, res.address FROM restaurants AS res LEFT JOIN (SELECT restaurant_id, AVG(rating) AS rest_rat FROM review GROUP BY restaurant_id) AS rat_ave ON res.id = rat_ave.restaurant_id ORDER BY COALESCE(rat_ave.rest_rat,0) DESC")
     result = db.session.execute(sql)
     return result.fetchall()
-
 
 def get_full_info(restaurant_id):
     sql = text("SELECT name, address, price_range, category, description FROM restaurants WHERE id=:restaurant_id")
@@ -31,12 +37,17 @@ def get_full_info(restaurant_id):
     return result.fetchone()
 
 def get_opening_hours(restaurant_id):
-    sql = text("SELECT day, open, close FROM opening_hours WHERE restaurant_id=:restaurant_id")
+    sql = text("SELECT day, open, close, status FROM opening_hours WHERE restaurant_id=:restaurant_id")
     result = db.session.execute(sql, {"restaurant_id":restaurant_id})
     return result.fetchall()
 
 
-def restaurant_id(name):
-    sql=text("SELECT id FROM restaurants WHERE name=:name")
-    result = db.session.execute(sql, {"name":name})
+def restaurant_id(address):
+    sql=text("SELECT id FROM restaurants WHERE address=:address")
+    result = db.session.execute(sql, {"address":address})
     return result.fetchone()[0]
+
+def remove_restaurant(restaurant_id):
+    sql=text("DELETE FROM restaurants WHERE id=:restaurant_id")
+    db.session.execute(sql, {"restaurant_id":restaurant_id})
+    db.session.commit()
