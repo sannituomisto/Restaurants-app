88affabd81445b33fbc8dab61fec4f03337d4a4a
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2023-03-05 16:23:58 +0200

    Updates to schema

--- schema.sql
+++ schema.sql
@@ -9,24 +9,31 @@ CREATE TABLE restaurants (
     id SERIAL PRIMARY KEY,
     name TEXT, 
     address TEXT UNIQUE,
-    price_range TEXT,
+    price_range INT,
     category TEXT,
     description TEXT
 );
 
 CREATE TABLE opening_hours (
     id SERIAL PRIMARY KEY,
-    restaurant_id INTEGER REFERENCES restaurants, 
+    restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE, 
     day INT,
-    open TIME,
-    close TIME
+    open TEXT,
+    close TEXT,
+    status TEXT
 );
 
 CREATE TABLE review(
     id SERIAL PRIMARY KEY,
-    restaurant_id INTEGER REFERENCES restaurants, 
-    username TEXT,
+    restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE, 
+    username TEXT REFERENCES users(username) ON DELETE CASCADE,
     comment TEXT,
     rating INT,
     time DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE favorites(
+    id SERIAL PRIMARY KEY,
+    username TEXT REFERENCES users(username) ON DELETE CASCADE,
+    restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE
 );
